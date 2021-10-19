ff2431806835dde1f4d15f70283f58a361e5b643
Rayeem Omayeer <rayeemomayeer@gmail.com>
2021-10-19 20:44:23 +0600

    footer and services added in home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -13,8 +12,17 @@ import ServiceDescription from './components/ServiceDescription/ServiceDescripti
 import Login from './components/Login/Login/Login';
 import AuthProvider from './contexts/AuthProvider';
 import PrivateRoute from './components/Login/PrivateRoute/PrivateRoute';
+import Footer from './components/Footer/Footer';
+import Service from './components/Service/Service';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [services, setServices] = useState([]);
+  useEffect(()=> {
+    fetch('services.json')
+      .then(result=>result.json())
+      .then(data=> setServices(data))
+  }, []);
   return (
     <div className="">
       <AuthProvider>
@@ -26,6 +34,17 @@ function App() {
           </Route>
           <Route path="/home">
             <Home></Home>
+            <div className="max-w-2xl mx-auto py-4 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+              <h1 className="text-5xl text-center mb-8 text-green-900">Our Services</h1>
+              <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                {
+                  services.slice(0,4).map(service=> <Service key={service.id} service={service}></Service>)
+                }
+              </div>
+            </div>
+            <div className="text-center">
+              <Link to="/services"><button className="btn btn-success">see all services</button></Link>
+            </div>
           </Route>
           <Route path="/services">
             <Services></Services>
@@ -40,6 +59,7 @@ function App() {
             
           </Route>
         </Switch>
+        <Footer></Footer>
       </Router>
       </AuthProvider>
     </div>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="footer-left-side">
+        <h2 style={{color: 'DarkBlue'}}>Contact us</h2>
+        <h5> Call Us</h5>
+        <p style={{color: 'SlateGray'}}>+38 90891 394628</p>
+        
+        <h5> Business hours</h5>
+        <p style={{color: 'SlateGray'}}>Mon -fri 8AM - 5PM</p>
+        
+        <h5> Location</h5>
+        <p style={{color: 'SlateGray'}}>Franklin st, Greenpoint ave</p>
+        
+      </div>
+      <div className="footer-right-side">
+        <h2 style={{color: 'DarkBlue'}}>Quick links</h2>
+        <Link style={{textDecoration: 'none',color: 'DodgerBlue'}} to="/home">Home</Link><br/><br/>
+        <Link style={{textDecoration: 'none',color: 'DodgerBlue'}} to="/classes">Classes</Link><br/><br/>
+        <Link style={{textDecoration: 'none',color: 'DodgerBlue'}} to="/services">Services</Link><br/><br/>
+        <Link style={{textDecoration: 'none',color: 'DodgerBlue'}} to="/teachers">Teachers</Link><br/><br/>
+        <Link style={{textDecoration: 'none',color: 'DodgerBlue'}} to="/about">About us</Link><br/><br/>
+      </div>
+      </div>
+      <p style={{textAlign: 'center'}}>&copy; Copyrights 2021 Speaker's Counsils All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
